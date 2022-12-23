60ea7641d74bd69392f1358beb05fb967491612d
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-12-23 18:53:13 +0900

    Time: 1 ms (53.29%), Space: 42.2 MB (89.46%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] == 0) nums1[i] = nums2[index++];
+            if (index == nums2.length) break;
+        }
+        Arrays.sort(nums1);
+    }
+}
